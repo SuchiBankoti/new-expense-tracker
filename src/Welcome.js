b89b16929d95b99f12f6867b1fe2107e994508ce
suchi <suchibankoti95@gmail.com>
2023-10-09 15:48:11 +0530

    add logout

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const profileApi = "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDvMhMxDWfRmYEbmRy4ORKoiOLsxpVokq0";
 const getProfileApi = "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDvMhMxDWfRmYEbmRy4ORKoiOLsxpVokq0"
-
-
+const getVerified="https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDvMhMxDWfRmYEbmRy4ORKoiOLsxpVokq0"
+const confirmVerificationCode="https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDvMhMxDWfRmYEbmRy4ORKoiOLsxpVokq0"
 export default function WelcomePage() {
     const [profile, setProfile] = useState(false)
     const [token, setToken] = useState(localStorage.getItem('token'))
@@ -49,6 +50,33 @@ export default function WelcomePage() {
         setToken(temp)
     }, [profile])
     
+    function verifyEmail() {
+        fetch(getVerified, {
+            method: "POST",
+            body: JSON.stringify({
+                requestType:"VERIFY_EMAIL",
+                idToken: token
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        .then(res => {
+            if (res.ok) {
+                    alert("Email Verified");
+                }
+           else {
+                alert("Email Verification Failed");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+        }
+
+        function logoutUser() {
+            localStorage.removeItem("token");
+          }
     function updateProfile() {
         fetch(profileApi, {
             method: "POST",
@@ -78,8 +106,14 @@ export default function WelcomePage() {
     }
     return (
         <div>
+            <div>
+                <Link to="/">
+                <button onClick={logoutUser}>Logout</button>
+                </Link>
+            </div>
             <div style={{display:"flex"}}>
-            <h1>Welcome To expense tracker</h1>
+                <h1>Welcome To expense tracker</h1>
+                <button className="verificationBtn" onClick={verifyEmail}>Verify Email</button>
             <div>Your profile is Incomplete.<span onClick={() => setProfile(true)} style={{color:"blue",cursor:"pointer"}}>Complete Now</span></div>
             </div>
             {
